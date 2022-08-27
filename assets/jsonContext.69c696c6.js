454e041843711dc74d8cddcd92b26794e8acc051
ziveo <bziveo@gmail.com>
2022-08-27 18:05:50 -0400

    Adding AppTitle

--- assets/jsonContext.69c696c6.js
+++
@@ -1,1 +0,0 @@
-import{bD as r,bC as e}from"./index.1985521f.js";function l(t){return{origin:"portal-item",url:r(t.itemUrl),portal:t.portal||e.getDefault(),portalItem:t,readResourcePaths:[]}}function a(t){return{origin:"portal-item",messages:[],writtenProperties:[],url:t.itemUrl?r(t.itemUrl):null,portal:t.portal||e.getDefault(),portalItem:t}}export{l as e,a as o};
